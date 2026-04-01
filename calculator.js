353aacfe294c383c53e6fdba803c0eb2d7763579
m3sh4al <aharbi419@gmail.com>
2026-04-01 09:36:00 +0300

    TODO2

--- calculator.js
+++ calculator.js
@@ -219,3 +219,7 @@ import { add, subtract, multiply, divide } from "./utils/operations.js";
 import { parseNumbers, isValidOperation } from "./utils/parser.js";
 import _ from "lodash";
 
+
+
+const operation = process.argv[2];
+const numbers = process.argv.slice(3);
